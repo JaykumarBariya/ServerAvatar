56e12a92c4c7c5283fcae167ffa9635c6ee97660
Jaykumar Bariya <87454980+JaykumarBariya@users.noreply.github.com>
2025-02-22 16:24:24 +0530

    Add files via upload

---
+++ mypage.php
@@ -0,0 +1,17 @@
+<?php
+    // Simple PHP Page
+    $title = "My Simple PHP Page";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h1>Welcome to <?php echo $title; ?></h1>
+    <p>This is a basic PHP webpage.</p>
+    <p>Current Date and Time: <?php echo date('Y-m-d H:i:s'); ?></p>
+</body>
+</html>
